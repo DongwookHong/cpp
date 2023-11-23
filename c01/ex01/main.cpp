7a71ad7b1ef128cafb0095a75019c82d19cf2610
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2023-11-23 10:09:24 +0000

    cpp 01

--- c01/ex01/main.cpp
+++ c01/ex01/main.cpp
@@ -3,9 +3,14 @@
 
 int main()
 {
-    int	zombie = 3;
-	Zombie* zb = zombieHorde(zombie, "Foooooo~");
+    int	zombie = -1;
 
+	Zombie* zb = zombieHorde(zombie, "Foooooo~");
+	if (!zb)
+	{
+		std::cout<<"Check argument"<<std::endl;
+		return 1;
+	}
 	for(int i = 0; i < zombie; i++)
 		zb[i].announce();
 
--- c01/ex01/zombieHorde.cpp
+++ c01/ex01/zombieHorde.cpp
@@ -4,8 +4,8 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-    if ( N <=0)
-      return ;
+    if ( N <= 0)
+      return NULL;
     Zombie *zb = new Zombie[N];
     for(int i = 0; i <N;i++)
     {
